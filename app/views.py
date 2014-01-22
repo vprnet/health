57e803bd4c4d99e501b548bcd7c98aaf49d46cf6
Matt Parrilla <matthew.parrilla@gmail.com>
2014-01-22 16:24:56 -0500

    Remove unused templates

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from config import BASE_URL
 @app.route('/')
 def index():
     page_url = BASE_URL + request.path
+    page_title = 'Health Care In Vermont'
     stories = api_feed([245948266, 186100276], numResults=10)
     most_recent = stories[:4]
     stories = stories[4:]
@@ -23,6 +24,7 @@ def index():
     }
 
     return render_template('home.html',
+        page_title=page_title,
         most_recent=most_recent,
         stories=stories,
         social=social,
@@ -33,6 +35,7 @@ def index():
 
 @app.route('/network')
 def network():
+    page_title = "Under The Hood: VT Health Connect"
     page_url = BASE_URL + request.path
     social = {'title': 'Vermont Health Connect: An Interactive Visualization',
         'subtitle': "How It Works and Where it Doesn't",
@@ -40,4 +43,7 @@ def network():
         'twitter_text': "What's wrong with VT Health Connect in one interactive graphic",
         'twitter_hashtag': "ACA",
         'description': "Walk through Vermont Health Connect from account creation to insurance confirmation, discovering how the system works and where it currently doesn't."}
-    return render_template('network.html', social=social, page_url=page_url)
+    return render_template('network.html',
+        page_title=page_title,
+        social=social,
+        page_url=page_url)
